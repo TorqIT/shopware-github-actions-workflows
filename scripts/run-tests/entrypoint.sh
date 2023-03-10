0e51a9551cc58184dc2751acfd6f598294719498
Evan Jackson <evan.jackson@torqit.ca>
2023-03-09 22:30:09 -0400

    Feat/better pr tests (#13)
    * Renaming
    
    * Encapsulating all the test logic here
    
    * Removing -it to make the runs non interactive
    
    * Run DB in background
    
    * Trying to fix entrypoint
    
    * Printing out that weird Github path
    
    * Typo
    
    * Moving entrypoint to caller
    
    * Relative path for entrypoint
    
    * Attempting a weird workaround
    
    * Just ls'ing GITHUB_WORKSPACE
    
    * Ls'ing some more
    
    * Ls'ing some more
    
    * Specifying ref
    
    * Hopefully got the right path now
    
    * Naming container
    
    * Using docker-compose
    
    * Comments and proper Dockerfile context
    
    * Requiring ref to be passed in
    
    * Fallback for ref
    
    * Comments

---
+++ scripts/run-tests/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+until nc -z -v -w30 $DATABASE_HOST 3306
+do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+
+echo Installing Pimcore...
+runuser -u www-data -- vendor/bin/pimcore-install \
+  --admin-username=test \
+  --admin-password=test \
+  --mysql-host-socket=$DATABASE_HOST \
+  --mysql-database=$DATABASE_NAME \
+  --mysql-username=$DATABASE_USER \
+  --mysql-password=$DATABASE_PASSWORD \
+
+echo Rebuilding classes...
+runuser -u www-data -- /var/www/html/bin/console pimcore:deployment:classes-rebuild -c -d -n
+
+echo Running tests...
+runuser -u www-data -- /var/www/html/vendor/bin/phpunit --testdox test
